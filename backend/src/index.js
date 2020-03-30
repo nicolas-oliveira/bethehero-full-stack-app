68716d6001460391565e0c07154bd03668afb51e
Nicolas <nicolasm15@hotmail.com>
2020-03-29 21:24:40 -0300

    Add src = controllers + database

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express'); //importar
+
+const routes = require('./routes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+// é necessário que as rotas estejam abaixo da execução do express
+app.use(routes);
+
+app.listen(3333);
+
