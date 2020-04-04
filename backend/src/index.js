588551e0dee79fcfb35d87a3a55a520961d88312
Nicolas <nicolasm15@hotmail.com>
2020-04-04 20:47:50 -0300

    Back-end development to axios lib

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,13 @@
 const express = require('express'); //importar
-
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 // é necessário que as rotas estejam abaixo da execução do express
 app.use(routes);
+
 
 app.listen(3333);
 
