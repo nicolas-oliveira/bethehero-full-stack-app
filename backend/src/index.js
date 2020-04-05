ba20f1c45a446d9c4cd2409f77d70171a3bcbf43
Nicolas <nicolasm15@hotmail.com>
2020-04-05 18:54:26 -0300

    Back-end Changes and finalization

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 const express = require('express'); //importar
-const routes = require('./routes');
 const cors = require('cors');
+const routes = require('./routes');
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 // é necessário que as rotas estejam abaixo da execução do express
--- backend/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create(
-    baseURL: 'htp://localhost:3333',
-);
-
-export default api;
