d584acea6b6754b6fb7e024e0fc7bc1242eb6f0a
Nicolas <nicolasm15@hotmail.com>
2020-04-17 17:59:57 -0300

    + Verificação dos esquemas via celebrate

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express'); //importar
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -8,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 // é necessário que as rotas estejam abaixo da execução do express
 app.use(routes);
-
+app.use(errors());
 
 app.listen(3333);
 
